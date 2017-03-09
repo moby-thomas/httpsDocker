0a92bd4b54a562396705ab45965ebf6f45ec2504
moby-thomas <thomas.moby79@gmail.com>
2017-03-09 17:28:25 +0000

    Final https PHP Projects

---
+++ src/actavo/public/index.php
@@ -0,0 +1,16 @@
+
+<html>
+<body>
+<h2> Actavo </h2>
+<?php 
+$pdo = new \PDO('mysql:host=db;dbname=mysql','root','admin');
+echo "Database Container and Webserver Container communicating properly";
+?>
+
+</body>
+</html> 
+
+
+
+
+
